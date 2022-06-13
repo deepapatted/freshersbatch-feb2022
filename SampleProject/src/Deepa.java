ea0cde8dab83ad9833ce65de1ee3995e977d1b86
deepapatted <deepapatted19@gmail.com>
2022-06-14 00:20:44 +0530

    push code to repo using eclipse git plug in

---
+++ SampleProject/src/Deepa.java
@@ -0,0 +1,10 @@
+
+public class Deepa {
+	public static void main(String[] args) {
+		System.out.println("Hello Deepa");
+		for (int i = 0; i < 4; i++) {
+			System.out.println("hi..Hey");
+			System.out.println("hi..Hey");
+		}
+	}
+}
